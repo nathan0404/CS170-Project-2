296850aa58d812fe1cade511e0297b5f726ef203
Nathan Matsuo <nathan@Nathans-MacBook-Air.local>
2025-03-18 17:50:07 -0700

    added basic file opening and knn, updated read me

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # CS170-Project-2
+
+documents used:
+#https://www.w3schools.com/python/python_ml_knn.asp used for knn
+#https://scikit-learn.org/stable/modules/generated/sklearn.neighbors.KNeighborsClassifier.html
+#https://stackoverflow.com/questions/15821116/how-to-measure-the-accuracy-of-knn-classifier-in-python
--- project_2.py
+++ project_2.py
@@ -1,1 +1,52 @@
-print("hi")
+import matplotlib.pyplot as plt
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+def knnAlgorithm(data, num):
+    X = list(zip(data[num]))
+    y = data[0]  
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2) #random_state=42
+    knn = KNeighborsClassifier()  
+    knn.fit(X_train, y_train)    
+    predicted = knn.predict(X_test)
+    acc = accuracy_score(y_test, predicted)
+    print(f"Using feature(s) {num} accuracy is {acc*100:.3f}%")
+
+def visualize(data):
+    x = data[1]
+    y = data[2]
+    classes = data[0]
+    plt.scatter(x, y, c=classes)
+    plt.show()
+
+def openingFile(fileName):
+    file = open(fileName, "r")
+    f=file.readline().split()
+    length = len(f)
+
+    features = []
+    for i in range(length): #creating arrays
+        features.append([])
+        print(features)
+    while True:
+        f=file.readline().split()
+        if not f:
+            break
+        for i in range(length):
+            features[i].append(float(f[i]))
+
+    #for i in range(len(features[0])):
+    #    print(features[0][i], features[1][i], features[2][i], features[3][i], features[4][i], features[5][i], features[6][i])
+
+    return features
+    
+
+def main():
+    data = openingFile("CS170_Small_Data__4.txt")
+    length = len(data)
+    for i in range(length):
+        knnAlgorithm(data, i)
+    return 
+
+main()
